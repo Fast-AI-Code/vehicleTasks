7510e0c052f9b1e5ba1001094f523efbb78620d2
Subhaditya <msubhaditya@gmail.com>
2019-12-09 01:58:29 +0530

    completed all tasks

--- crash.py
+++ crash.py
@@ -1,3 +1,68 @@
-import cv2
 import numpy as np
+import imutils
+import dlib
+import cv2
+import time
 
+classes = [
+    "background", "aeroplane", "bicycle", "bird", "boat", "bottle", "bus",
+    "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike",
+    "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"
+]
+
+net = cv2.dnn.readNetFromCaffe("data/model.prototxt", "data/ssd.caffemodel")
+frame = cv2.imread("data/scr.png")
+frame = imutils.resize(frame, width=600)
+frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+(h, w) = frame.shape[:2]
+createBlob = cv2.dnn.blobFromImage(frame, 0.007843, (w, h), 127.5)
+net.setInput(createBlob)  #using dnn
+detected = net.forward()  # updating the blob
+
+cars_list_temp = []
+cno = 1
+for i in np.arange(0, detected.shape[2]):
+    conf = detected[0, 0, i, 2]
+
+    if conf > 0.5:  # confidence of object detection
+        idx = int(detected[0, 0, i, 1])
+        label = classes[idx]
+        if label != "car":  # only check for cars and ignore rest
+            continue
+
+        box = detected[0, 0, i, 3:7] * np.array([w, h, w, h])
+        (startX, startY, endX, endY) = box.astype("int")
+        cv2.rectangle(frame, (startX, startY), (endX, endY), (255, 255, 0), 2)
+        cars_list_temp.append((startX, startY, endX, endY))  #xywh
+        cv2.putText(frame, str(cno), (startX, startY - 5),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.45, (255, 255, 0), 2)
+        cno += 1
+
+# Assuming car 1 (left) wants to take a turn right now
+
+test_car1, test_car2 = cars_list_temp[0], cars_list_temp[
+    1]  # Taking 1st two cars
+x_car1, x_car2 = test_car1[0], test_car2[0]  # taking x coord of both cars
+# x_car1, x_car2 = test_car2[0], test_car1[0]  # taking x coord of both cars
+
+thresh = test_car1[2] + 5  # say traffic regulation (w+thresh)
+
+# Assuming road boundaries here are the left and right corners of the image
+left_boun, right_bound = 0, w
+print(x_car1, left_boun,thresh)
+# to check for car 1 left turn
+if x_car1 - left_boun > thresh:
+    cv2.putText(frame, "car 1 go left", (int(w / 2), int(h / 2)),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.45, (255, 255, 0), 2)
+    print(1)
+elif all([right_bound - x_car1 > thresh, x_car2 - x_car1 > thresh]):
+    cv2.putText(frame, "car 1 go right", (int(w / 2), int(h / 2)),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.45, (255, 255, 0), 2)
+    print(2)
+else:
+    cv2.putText(frame, "car 1 go straight", (int(w / 2), int(h / 2)),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.45, (255, 255, 0), 2)
+    print(3) # change thresh to a high value
+
+cv2.imshow("det", frame)
+cv2.waitKey(0)
--- speedDetect.py
+++ speedDetect.py
@@ -2,68 +2,124 @@ import numpy as np
 import imutils
 import dlib
 import cv2
+import math
+import time
 
+# all classes from ssd needed for recognition
 classes = [
     "background", "aeroplane", "bicycle", "bird", "boat", "bottle", "bus",
     "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike",
     "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"
 ]
 
-net = cv2.dnn.readNetFromCaffe("data/model.prototxt", "data/ssd.caffemodel")
+net = cv2.dnn.readNetFromCaffe("data/model.prototxt",
+                               "data/ssd.caffemodel")  # pretrained SSD
 
 video = cv2.VideoCapture("data/clipped.mp4")
 
-trackers, labels = [], []
+# trackers, labels = [], []
+carLoc1, carLoc2, carTracker, timer = {}, {}, {}, {
+}  # initialize car locations
+currentId = 0
+dist = 16  #real distance - random value assumed
+speed_limit = 70
 
 while True:
     (grabbed, frame) = video.read()
+
     if frame is None:
         break
-
     frame = imutils.resize(frame, width=600)
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    if len(trackers) == 0:
+
+    toDel = []  # creating sep list to prevent dict size change error
+    for cid in carTracker.keys():
+        trackingQuality = carTracker[cid].update(frame)
+        if trackingQuality < 6:
+            toDel.append(cid)
+
+    for a in toDel:
+        carTracker.pop(a, None)
+        carLoc1.pop(a, None)
+        carLoc2.pop(a, None)
+
+    if len(carTracker) == 0:  # basically if empty tracker list
         (h, w) = frame.shape[:2]
+        distPerPixel = dist / w
         createBlob = cv2.dnn.blobFromImage(frame, 0.007843, (w, h), 127.5)
-        net.setInput(createBlob)
-        detected = net.forward()
+        net.setInput(createBlob)  #using dnn
+        detected = net.forward()  # updating the blob
 
         for i in np.arange(0, detected.shape[2]):
             conf = detected[0, 0, i, 2]
-            if conf > 0.5:
+
+            if conf > 0.5:  # confidence of object detection
                 idx = int(detected[0, 0, i, 1])
                 label = classes[idx]
-                if label != "car":
+                if label != "car":  # only check for cars and ignore rest
                     continue
 
                 box = detected[0, 0, i, 3:7] * np.array([w, h, w, h])
                 (startX, startY, endX, endY) = box.astype("int")
 
-                t = dlib.correlation_tracker()
+                t = dlib.correlation_tracker()  # use dlib tracker
                 rect = dlib.rectangle(startX, startY, endX, endY)
                 t.start_track(frame, rect)
+                carTracker[currentId] = t  # specific id per car
+                carLoc1[currentId] = [startX, startY, endX, endY]
+                currentId += 1
 
-                labels.append(label)
-                trackers.append(t)
+                # labels.append(label)
+                # trackers.append(t)
                 cv2.rectangle(frame, (startX, startY), (endX, endY),
                               (255, 255, 0), 2)
                 cv2.putText(frame, label, (startX, startY - 15),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.45, (255, 255, 0), 2)
-    else:
-        for (t, l) in zip(trackers, labels):
-            quality_track = t.update(frame)
-            if quality_track<6:
-                trackers.remove(t)
-                labels.remove(l)
-            pos = t.get_position()
+    else:  # if already there then -> update existing tracker
+        # for (t, l) in zip(trackers, labels):
+        #     quality_track = t.update(frame)
+
+        #     pos = t.get_position()
+        #     startX, startY, endX, endY = int(pos.left()), int(pos.top()), int(
+        #         pos.right()), int(pos.bottom())
+        #     carLoc2[currentId]= [startX, startY, endX, endY]
+        #     cv2.rectangle(frame, (startX, startY), (endX, endY), (255, 255, 0),
+        #                   2)
+        #     cv2.putText(frame, l, (startX, startY - 15),
+        #                 cv2.FONT_HERSHEY_SIMPLEX, 0.45, (255, 255, 0), 2)
+
+        for cid in carTracker.keys():
+            car = carTracker[cid].update(frame)
+            pos = carTracker[cid].get_position()
             startX, startY, endX, endY = int(pos.left()), int(pos.top()), int(
                 pos.right()), int(pos.bottom())
+            carLoc2[currentId] = [startX, startY, endX, endY]
             cv2.rectangle(frame, (startX, startY), (endX, endY), (255, 255, 0),
                           2)
-            cv2.putText(frame, l, (startX, startY - 15),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.45, (255, 255, 0), 2)
+
+    # # calculate speed
+    for cid in carTracker.keys():
+        # print(carTracker, carLoc2, carLoc1)
+        try:
+            dist_pixels = math.sqrt(
+                math.pow(carLoc2[cid][0] - carLoc1[cid][0], 2) +
+                math.pow(carLoc2[cid][1] - carLoc1[cid][1], 2))
+            pixels_per_meter = 30
+            dist_meters = dist_pixels / pixels_per_meter
+            fps = 10
+            speed = dist_meters * fps  # speed in meters per second
+            cv2.putText(frame, str(speed), (int(carLoc2[cid][0]), int(carLoc2[cid][1])),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.45, (255, 255, 0), 2)
+            if speed> speed_limit:
+                print(speed, cid)
+                cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 0, 255),
+                          2)
+
+        except KeyError:
+            pass
 
     cv2.imshow("det", frame)
+    # print(carLoc1)
     key = cv2.waitKey(1) & 0xFF
     if key == ord("q"):
         break
